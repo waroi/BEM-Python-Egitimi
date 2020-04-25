2437ec55bb490efaa81b073b9b0c85d0211ba0a0
Varol Maksutoğlu <waroi@pisilinux.org>
2020-04-25 17:56:28 +0300

    Ders04 ve Quiz2 Soruları

---
+++ Ders04.py
@@ -0,0 +1,73 @@
+# print("Python")
+# print("Python")
+# print("Python")
+# print("Python")
+# print("Python")
+# print("Python")
+# print("Python")
+# print("Python")
+# print("Python")
+# print("Python")
+
+#while döngüsü
+# a=1
+# while(a<=10):
+#   print("Python")
+#   a+=1 # a=a+1
+
+# while True:  # sonsuz döngü
+#   print("Python")
+
+# # for döngüsü
+# for x in "python":
+#   print(x)
+
+# liste=[5,4,3,2,1]
+# for sayi in liste:
+#   print("Sayı: ",sayi)
+#   print("***")
+# print("-------------------------")
+
+# demet=(5,4,3,2,1)
+# for sayi in demet:
+#   print("Sayı: ",sayi)
+#   print("***")
+# print("-------------------------")
+
+# t=[(10,9,8),(7,6,5),(4,3,2)]
+# for a,b,c in t:
+#   print("Toplam: ",a+b+c)
+
+# gunler={"1.gün":"pazartesi","2.gün":"salı","3.gün":"çarşamba","4.gün":"perşem","5.gün":"cuma"}
+# for gun in gunler:
+#   print(gunler[gun])
+
+# gunler={"1.gün":"pazartesi","2.gün":"salı","3.gün":"çarşamba","4.gün":"perşem","5.gün":"cuma"}
+# for gun in gunler.values():
+#   print(gun)
+
+# gunler={"1.gün":"pazartesi","2.gün":"salı","3.gün":"çarşamba","4.gün":"perşem","5.gün":"cuma"}
+# for key,val in gunler.items():
+#   print(key,val)
+
+# gunler={"1.gün":"pazartesi","2.gün":"Salı","3.gün":"Çarşamba","4.gün":"Perşembe","5.gün":"Cuma"}
+# for gun in gunler.values():
+#     print(gun)
+
+# print(*range(5,20,2))
+
+# for sayi in range(0,10):
+#   print("Pyton",sayi,sep="",end=" ")
+
+# liste=[1,2,3,4,5,6,7,8,9]
+# for sayi in liste:
+#   if (sayi==7):
+#     break
+#   print(sayi)
+
+# liste=[1,2,3,4,5,6,7,8,9]
+# for sayi in liste:
+#   if (sayi==7):
+#     continue
+#   print(sayi)
+
