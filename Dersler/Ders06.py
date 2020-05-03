ce8966a072522cc1dade7ae29fc1754423df82aa
Varol Maksutoğlu <waroi@pisilinux.org>
2020-05-03 18:09:02 +0300

    Ders06 ve PyQuiz05 eklendi

---
+++ Dersler/Ders06.py
@@ -0,0 +1,41 @@
+# Gömülü Fonksiyonalar
+# a=10
+# print(type(a))
+
+# map() fonksiyonu
+# def carp(x):
+#   return x*2
+# a=map(carp, [1,2,3,4,5,6,7,8,9])
+# print(list(a))
+
+# lambda ile map kullanımı
+# a=map(lambda x:x*2,[1,2,3,4,5])
+# print(list(a))
+
+# liste1=[1,2,3,4,5]
+# liste2=[6,7,8,9,10]
+# liste3=[3,1,6,4,2]
+# carpim=map(lambda x,y,z:x*y*z,liste1,liste2,liste3)
+# print(list(carpim))
+
+# reduce() Fonksiyonu
+# from functools import reduce
+# toplam=reduce(lambda x,y:x+y,[1,2,3,4,5])
+# print(toplam)
+
+# filter() Fonksiyonu
+# print(list(filter(lambda x:x%2==0,range(1,11))))
+
+# zip() onksiyonu
+# liste1=[1,2,3,4,5]
+# liste2=[6,7,8,9,10]
+# liste3=["Ankara","İstanbul","Giresun","Muğla","İzmir"]
+# print(list(zip(liste1,liste2,liste3)))
+
+# all() Fonksiyonu bir tane bile false olursa Flase döner
+# liste=[1,2,3,4,5,"Python",0]
+# print(all(liste))
+
+# any() Fonksiyonu bir tane bile true olsa True döner
+# liste=[1,2,3,4,5,"Python"]
+# print(any(liste))
---
+++ PyQuiz/PyQuiz05.py
@@ -0,0 +1,28 @@
+# Problem 1
+# Elinizde bir dikdörtgenin kenarlarını ifade eden sayı çiftlerinin bulunduğu bir liste olsun.
+#          [(3,4),(10,3),(5,6),(1,9)]
+# Şimdi kenar uzunluklarına göre dikdörtgenin alanını hesaplayan bir fonksiyon yazın ve bu listenin her 
+# bir elemanına bu fonksiyonu uygulayarak ekrana şöyle bir liste yazdırın.
+#          [12, 30, 30, 9]
+# Not : map() fonksiyonunu kullanmaya çalışın.
+
+# Problem 2
+# Elinizden her bir elemanı 3'lü bir demet olan bir liste olsun.
+#      [(3,4,5),(6,8,10),(3,10,7)]
+# Şimdi kenar uzunluklarına göre bu kenarların bir üçgen olup olmadığını dönen bir fonksiyon yazın ve 
+# sadece üçgen belirten kenarları bulunduran listeyi ekrana yazdırın.
+#      [(3, 4, 5), (6, 8, 10)]
+# Not: filter() fonksiyonunu kullanmaya çalışın.
+
+# Problem 3
+# Elinizde şöyle bir liste bulunsun.
+#     [1,2,3,4,5,6,7,8,9,10]
+# Bu listenin içindeki çift sayıların toplamını ekrana yazdıran bir fonksiyon yazın.
+# Not: İlk önce filter() fonksiyonu ile çift sayıları ayıklayın. Daha sonra reduce() fonksiyonunu kullanın.
+
+# Problem 4
+# Elinizde isimlerin ve soyisimlerin bulunduğu iki tane liste olsun.
+#         isimler -----> ["Kerim","Tarık","Ezgi","Kemal","İlkay","Şükran","Merve"]
+#         soyisimler -----> ["Yılmaz","Öztürk","Dağdeviren","Atatürk","Dikmen","Kaya","Polat"]
+# Bu isimleri ve soyisimleri sırasıyla eşleştirin ve ekrana alt alta isimleri ve soyisimleri yazdırın.
+# Not: zip() fonksiyonunu kullanmaya çalışın.
